ef82dc892d82e99d0fd2d00295cc9935fe6eb7b7
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-05 22:43:56 +0700

    Add HealthDisplay UI component
    Introduces a HealthDisplay MonoBehaviour for managing and displaying health icons in the UI. The component dynamically adds or removes health icons based on the Points property.

---
+++ Assets/Scripts/UI/HealthDisplay.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] GameObject healthIconPrefab;
+    [SerializeField] List<GameObject> icons;
+
+    [SerializeField] private int points = 0;
+
+    public int Points
+    {
+        get => points;
+        set
+        {
+            int oldValue = points;
+            points = value;
+
+            ManageIcon(value-oldValue);
+        }
+    }
+    void ManageIcon(int deltaPoint)
+    {
+        if (deltaPoint == 0)
+        {
+            return;
+        }
+
+        if (deltaPoint > 0)
+        {
+            for (int i = 0; i < deltaPoint; i++)
+            {
+                var newIcon = Instantiate(healthIconPrefab, transform);
+
+                icons.Add(newIcon);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < -deltaPoint; i++)
+            {
+                var icon = icons[icons.Count - 1];
+
+                icons.Remove(icon);
+
+                Destroy(icon);
+            }
+        }
+
+
+
+
+    }
+}
