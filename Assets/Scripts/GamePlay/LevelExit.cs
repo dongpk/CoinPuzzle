c97b9172b2a86cdb397b7ff38e6b6cf13560ee91
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-05 22:42:07 +0700

    Add LevelExit script for gate and scene transition
    Introduces LevelExit.cs, a Unity script to handle opening a gate and transitioning to a target scene. The script manages gate animation and disables the collider when the gate opens.

---
+++ Assets/Scripts/GamePlay/LevelExit.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] Collider gateCollider;
+    [SerializeField] Transform gateMesh;
+    [SerializeField] float speedOpenGate = 2f;
+    [SerializeField] string targetScene;
+    bool openGate = false;
+
+    public void OpenGate()
+    {
+        openGate = true;
+
+        gateCollider.enabled = false;
+
+    }
+    public void ExitLevel()
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+    private void Update()
+    {
+        if (openGate)
+        {
+            Vector3 targetPosition = new Vector3(0, 0.75f, 0);
+            gateMesh.localPosition = Vector3.Lerp(gateMesh.localPosition, targetPosition, speedOpenGate * Time.deltaTime);
+        }
+    }
+}
