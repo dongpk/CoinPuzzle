2e31a7ddd5f2dc7c27e0b4be3c21c2ee9a1d6eff
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-05 22:42:17 +0700

    Add LevelProgressTracker to manage coin collection
    Introduces LevelProgressTracker script to track remaining coins in a level, update the UI, and open the level exit when all coins are collected.

---
+++ Assets/Scripts/GamePlay/LevelProgressTracker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProgressTracker : MonoBehaviour
+{
+    [SerializeField] List<GameObject> coins;
+    [SerializeField] LevelExit levelExit;
+    public int remainingCoins => coins.Count;
+
+    private void Start()
+    {
+        coins = new List<GameObject>(GameObject.FindGameObjectsWithTag("Coin"));
+
+    }
+
+    private void Update()
+    {
+        int previousRemainingCoins = remainingCoins;
+
+        //xóa null coin khỏi danh sách
+        for (int i = coins.Count - 1; i >= 0; i--)
+        {
+            var coin = coins[i];
+            if (coin == null)
+            {
+                coins.RemoveAt(i);
+            }
+        }
+
+        //cap nhật UI
+        UIManager.Instance.progressUI.SetRemaingCoinLabel(remainingCoins);
+
+
+        if (previousRemainingCoins != remainingCoins && remainingCoins == 0)
+        {
+            levelExit.OpenGate();
+        }
+    }
+}
