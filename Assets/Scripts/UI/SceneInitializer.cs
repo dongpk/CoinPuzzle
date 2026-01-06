6e9bc4065fc4f6596540c894fc2efa246fc9470c
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-06 22:37:39 +0700

    add scene init

---
+++ Assets/Scripts/UI/SceneInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class SceneInitializer : MonoBehaviour
+{
+    public enum SceneType
+    {
+        Gameplay,
+        MainMenu
+    }
+    [SerializeField] SceneType sceneType=SceneType.Gameplay;
+    [SerializeField] UIManager uiManagerPrefab;
+
+    private void Awake()
+    {
+        if (UIManager.Instance == null)
+        {
+            Instantiate(uiManagerPrefab);   
+            DontDestroyOnLoad(UIManager.Instance);
+        }
+    }
+
+    private void Start()
+    {
+        if(sceneType==SceneType.Gameplay)
+        {
+            UIManager.Instance.ShowHUD();
+        }
+        else if(sceneType==SceneType.MainMenu)
+        {
+            UIManager.Instance.ShowMainMenu();
+        }
+    }
+
+}
