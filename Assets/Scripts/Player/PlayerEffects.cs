f9dd2af3547f5749dfd8c22b04787d976f0a1866
Trần Hải Đông <Tranhaidongpk@gmail.com>
2026-01-07 21:47:25 +0700

    Add sprint effects to PlayerEffects script
    Introduced sprint start particle effect and trail rendering for sprinting. Added methods to handle activation and deactivation of sprint effects, and initialized trail state in Start().

--- Assets/Scripts/Player/PlayerEffects.cs
+++ Assets/Scripts/Player/PlayerEffects.cs
@@ -5,11 +5,32 @@ using UnityEngine;
 public class PlayerEffects : MonoBehaviour
 {
     [SerializeField] private GameObject landingFX;
+    [SerializeField] private ParticleSystem sprintStartFX;
+    [SerializeField] private TrailRenderer[] sprintTrail;
 
 
+    private void Start()
+    {
+        SetSprintTrailActive(false);
+    }
     public void OnLanded()
     {
         Instantiate(landingFX, transform.position, Quaternion.identity);
     }
-   
+    public void OnSprintStarted()
+    {
+        sprintStartFX.Play();
+        SetSprintTrailActive(true);
+    }
+    public void OnSprintFinished()
+    {
+        SetSprintTrailActive(false);
+    }
+    void SetSprintTrailActive(bool active)
+    {
+        foreach (var trail in sprintTrail)
+        {
+            trail.emitting = active;
+        }
+    }
 }
